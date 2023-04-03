bf5d88f7878dea1b848a5364cb8bfe11c917ce76
rez06 <pelobello.rezyl@gmail.com>
2023-04-03 19:07:48 +0800

    Added about and contact page

---
+++ index.js
@@ -0,0 +1,19 @@
+function changeNavbarColorOnScroll() {
+  let navbar = document.getElementById("navbar");
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+}
+
+window.addEventListener("scroll", changeNavbarColorOnScroll);
+
+// function scrollToSection(e) {
+//   e.preventDefault();
+//   const section = document.querySelector('.main-container');
+//   section.scrollIntoView({ behavior: 'smooth' });
+// }
+
+// const scrollBtn = document.querySelector('#scrollBtn');
+// scrollBtn.addEventListener('click', scrollToSection);
